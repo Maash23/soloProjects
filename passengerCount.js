45def4608ae8fcabe7f14391d1830f5e50bd5306
Maash23 <ahmedmaash.official@gmail.com>
2026-03-24 12:44:28 +0500

    Add files via upload

---
+++ passengerCount.js
@@ -0,0 +1,24 @@
+//document.getElementById('count-el').innerText = 9;
+
+let count = 0;
+
+countEl = document.getElementById('count-el');
+saveEl = document.getElementById('save-el');
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+};
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+};
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    
+    countEl.textContent = 0
+    count = 0
+};
